2ed76ab5cd2caa7a05868993ae1cf05bcf3896b4
E-L-T <ericleroyterquem@yahoo.fr>
2017-06-20 12:30:27 +0200

    Factorisation du script précédent. Le programme permet désormais de filtrer tous les projets par catégorie

--- scriptFiltres.js
+++ scriptFiltres.js
@@ -1,14 +1,34 @@
-var filtreExposition = document.getElementById("filtreExposition");
-filtreExposition.addEventListener("click", function() {
+function filtrerSommaire(filtre, realisationChoix) {
+    var filtreElt = document.getElementById(filtre);
+    filtreElt.addEventListener("click", function() {
+        var realisationsElts = document.getElementById("realisations");
+        var integraleRealisationElts = realisationsElts.querySelectorAll(".realisations, .realisation, .exposition, .dessin, .installation, .sculpture, .livret");
+        for (i = 0; i < integraleRealisationElts.length; i ++) {
+            integraleRealisationElts[i].style.display = "block";
+        }
+        var excludedElts = realisationsElts.querySelectorAll("div:not(." + realisationChoix + ")"); //regarder la fin de cette ligne à tête reposée
+        for (i = 0; i < excludedElts.length; i++) {
+            excludedElts[i].style.display = "none";
+        }
+        var texteRealisationsElts = document.querySelectorAll(".texteRealisation");
+        for (j = 0; j < texteRealisationsElts.length; j++) {
+            texteRealisationsElts[j].style.display = "block";
+        }
+    });
+}
+
+filtrerSommaire("filtreExposition", "exposition");
+filtrerSommaire("filtreDessin", "dessin");
+filtrerSommaire("filtreInstallation", "installation");
+filtrerSommaire("filtreSculpture", "sculpture");
+filtrerSommaire("filtreLivret", "livret");
+
+//gestionnaire d'evt pour afficher tout à nouveau
+var filtreToutElt = document.getElementById("filtreTout");
+filtreToutElt.addEventListener("click", function() {
     var realisationsElts = document.getElementById("realisations");
-    var notExpositionElts = realisationsElts.querySelectorAll("div:not(.exposition)"); 
-    for (i = 0; i < notExpositionElts.length; i++) {
-        notExpositionElts[i].style.display = "none";
+    var integraleRealisationElts = realisationsElts.querySelectorAll(".realisations, .realisation, .exposition, .dessin, .installation, .sculpture, .livret");
+    for (i = 0; i < integraleRealisationElts.length; i ++) {
+        integraleRealisationElts[i].style.display = "block";
     }
-    var texteRealisationsElts = document.querySelectorAll(".texteRealisation");
-    for (j = 0; j < texteRealisationsElts.length; j++) {
-        texteRealisationsElts[j].style.display = "block";
-    }
-    filtreExposition.style.color = "rgba(253,0,141,0.8)";
-    filtreExposition.style.textDecoration = "underline";
 });
